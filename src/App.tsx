76884012ccea476e00e9db47e0ead47787a86316
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-11-21 13:59:29 -0300

    some progress on progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,38 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-
-import { Stats, Flashcard, NavBar } from "@/components";
+import { Flashcard, NavBar } from "@/components";
 
 export default function App(): JSX.Element {
   return (
-    <div className="">
-      <Tabs defaultValue="fleshcard" className="">
-        <NavBar>
-          <TabsList>
-            <TabsTrigger value="fleshcard">Fleshcard</TabsTrigger>
-            <TabsTrigger value="stats" disabled>
-              Stats
-            </TabsTrigger>
-          </TabsList>
-        </NavBar>
-
-        <TabsContent value="stats" className="">
-          <Stats />
-        </TabsContent>
-        <TabsContent
-          value="fleshcard"
-          className="space-y-4 flex justify-center overflow-hidden"
-        >
-          <Flashcard />
-        </TabsContent>
-      </Tabs>
+    <div>
+      <NavBar />
+      <div className="space-y-4 flex justify-center overflow-hidden">
+        <Flashcard />
+      </div>
     </div>
   );
 }
+
+// Old tab style
+// return (
+//   <div className="">
+//     <Tabs defaultValue="fleshcard" className="">
+//       <NavBar>
+//         <TabsList>
+//           <TabsTrigger value="fleshcard">Fleshcard</TabsTrigger>
+//           <TabsTrigger value="stats" disabled>
+//             Stats
+//           </TabsTrigger>
+//         </TabsList>
+//       </NavBar>
+
+//       <TabsContent value="stats" className="">
+//         <Stats />
+//       </TabsContent>
+//       <TabsContent
+//         value="fleshcard"
+//         className="space-y-4 flex justify-center overflow-hidden"
+//       >
+//         <Flashcard />
+//       </TabsContent>
+//     </Tabs>
+//   </div>
+// );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,30 +4,22 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ArrowRight } from "lucide-react";
 import { useState } from "react";
+import { Progress } from "@/components/ui/progress";
 
-export function NavBar({ children }: { children: JSX.Element }): JSX.Element {
-  // const [lastScrollTop, setLastScrollTop] = useState(0);
-  // const [visible, setVisible] = useState(true);
+export function DonationButton(): JSX.Element {
+  const [isDialogShowing, setDialogShow] = useState(false);
+  const onShowDialog = (): void => {
+    setDialogShow(!isDialogShowing);
+  };
 
-  // const handleScroll = (): void => {
-  //   const currentScrollTop = document.documentElement.scrollTop;
-  //   if (currentScrollTop > lastScrollTop) {
-  //     // Scroll Down
-  //     setVisible(!visible);
-  //   } else {
-  //     // Scroll Up
-  //   }
-  //   setLastScrollTop(currentScrollTop);
-  // };
+  return (
+    <Button variant={"ghost"} onClick={onShowDialog}>
+      🥟
+    </Button>
+  );
+}
 
-  // useEffect(() => {
-  //   window.addEventListener("scroll", handleScroll, { passive: true });
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [lastScrollTop]);
-  // ${ visible ? "translate-y-0" : "-translate-y-full" }
-
+export function NavBar({ children }: { children?: JSX.Element }): JSX.Element {
   function InputWithButton(): JSX.Element {
     const { currentCard, setCurrentCard } = useCardCounter();
     const [inputValue, setInputValue] = useState("");
@@ -59,9 +51,8 @@ export function NavBar({ children }: { children: JSX.Element }): JSX.Element {
       className={` w-full px-4 bg-secondary top-0 transition-transform duration-300 ease-in-out`}
     >
       <div className="flex h-16 items-center justify-between">
-        <div>🥟</div>
-        <div className="flex-1 flex justify-center">{children}</div>
-        <div className="ml-auto flex items-center space-x-4">
+        <Progress className="w-28" value={100} />
+        <div className="flex items-center space-x-4">
           <InputWithButton />
           <ModeToggle />
         </div>
@@ -69,3 +60,25 @@ export function NavBar({ children }: { children: JSX.Element }): JSX.Element {
     </div>
   );
 }
+
+// const [lastScrollTop, setLastScrollTop] = useState(0);
+// const [visible, setVisible] = useState(true);
+
+// const handleScroll = (): void => {
+//   const currentScrollTop = document.documentElement.scrollTop;
+//   if (currentScrollTop > lastScrollTop) {
+//     // Scroll Down
+//     setVisible(!visible);
+//   } else {
+//     // Scroll Up
+//   }
+//   setLastScrollTop(currentScrollTop);
+// };
+
+// useEffect(() => {
+//   window.addEventListener("scroll", handleScroll, { passive: true });
+//   return () => {
+//     window.removeEventListener("scroll", handleScroll);
+//   };
+// }, [lastScrollTop]);
+// ${ visible ? "translate-y-0" : "-translate-y-full" }
