4f36926b5c3092309e2aabcded31e885a8479428
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2023-11-23 18:35:34 -0300

    1

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,35 @@
 import path from "path";
 import { defineConfig } from "vite";
-import { VitePWA } from "vite-plugin-pwa";
-import vercel from "vite-plugin-vercel";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    vercel(),
-    VitePWA({
-      registerType: "prompt",
-      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
-      manifest: {
-        name: "Tranquilla Drive",
-        short_name: "TranqueDrive",
-        description: "Flashcards for your tranquilla journey",
-        theme_color: "#ffffff",
-        display: "standalone",
-        orientation: "portrait",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: "prompt",
+    //   includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+    //   manifest: {
+    //     name: "Tranquilla Drive",
+    //     short_name: "TranqueDrive",
+    //     description: "Flashcards for your tranquilla journey",
+    //     theme_color: "#ffffff",
+    //     display: "standalone",
+    //     orientation: "portrait",
+    //     icons: [
+    //       {
+    //         src: "pwa-192x192.png",
+    //         sizes: "192x192",
+    //         type: "image/png",
+    //       },
+    //       {
+    //         src: "pwa-512x512.png",
+    //         sizes: "512x512",
+    //         type: "image/png",
+    //       },
+    //     ],
+    //   },
+    // }),
   ],
   resolve: {
     alias: {
